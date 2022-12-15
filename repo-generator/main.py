a2c5508156258d8672c0663a1ecdbfde9a06fd3c
Artur Rodrigues <artur.rodrigues@lacework.net>
2022-12-15 12:59:30 +0000

    Work on several charts

--- repo-generator/main.py
+++ repo-generator/main.py
@@ -22,35 +22,39 @@ def generate_file_contents(line_num: int = 50) -> str:
     return "\n".join(lines)
 
 
-def modify_random_file() -> str:
-    files = os.listdir(TRASH_DIR_PATH)
-    chosen_file = random.choice(files)
-    file_path = f"{TRASH_DIR_PATH}/{chosen_file}"
-    with open(file_path, "w") as f:
-        f.write(generate_file_contents())
-    return file_path
-
-
-def create_random_file() -> str:
-    file_name = generate_random_message(1).lower()
-    file_path = f"{TRASH_DIR_PATH}/{file_name}"
-    with open(file_path, "w") as f:
-        f.write(generate_file_contents())
-    return file_path
-
-
 def git_add_and_commit(file_path: str) -> None:
     subprocess.run(f"git add {file_path}", shell=True)
     msg = generate_random_message()
     subprocess.run(f"git commit --no-gpg-sign -m '{msg}'", shell=True)
 
 
+class Generator:
+    def __init__(self, trash_dir_path: str):
+        self.trash_dir_path = trash_dir_path
+
+    def modify_random_file(self) -> str:
+        files = os.listdir(self.trash_dir_path)
+        chosen_file = random.choice(files)
+        file_path = f"{self.trash_dir_path}/{chosen_file}"
+        with open(file_path, "w") as f:
+            f.write(generate_file_contents())
+        return file_path
+
+    def create_random_file(self) -> str:
+        file_name = generate_random_message(1).lower()
+        file_path = f"{self.trash_dir_path}/{file_name}"
+        with open(file_path, "w") as f:
+            f.write(generate_file_contents())
+        return file_path
+
+
 if __name__ == "__main__":
     initialize_words()
-    for _ in range(50):
-        file = create_random_file()
-        git_add_and_commit(file)
-
-    for _ in range(500):
-        file = modify_random_file()
-        git_add_and_commit(file)
+    charts = ["helm-guestbook1", "helm-guestbook2"]
+    for chart in charts:
+        gen = Generator(f"{chart}/trash")
+        for _ in range(10):
+            file = gen.create_random_file()
+            git_add_and_commit(file)
+            file = gen.modify_random_file()
+            git_add_and_commit(file)
